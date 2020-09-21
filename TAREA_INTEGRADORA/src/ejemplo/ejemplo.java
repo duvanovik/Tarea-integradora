9fdd365ab91ab32e9713fb01f6ba60d30bb52dfc
prestamo <prestamo@SCM06E17>
2020-09-20 22:42:34 -0500

    commit 2 ejemeplo

--- TAREA_INTEGRADORA/src/ejemplo/ejemplo.java
+++ TAREA_INTEGRADORA/src/ejemplo/ejemplo.java
@@ -1,5 +1,13 @@
 package ejemplo;
 
+import javax.swing.JOptionPane;
+
 public class ejemplo {
 
+	
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Holaaaaaaaaaaaaaaaaaaaaaa");
+	}
+	
 }
