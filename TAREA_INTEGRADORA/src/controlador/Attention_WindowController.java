94dca5ddc8f5a5ac3a1414477fe99797b4dd889c
andrescuellar123 <47890673+andrescuellar123@users.noreply.github.com>
2020-09-23 21:31:33 -0500

    Algunas excepciones

--- TAREA_INTEGRADORA/src/controlador/Attention_WindowController.java
+++ TAREA_INTEGRADORA/src/controlador/Attention_WindowController.java
@@ -27,118 +27,130 @@ import model.Client;
 
 public class Attention_WindowController{
 
-    @FXML
-    private TextField tfCedulaBusqueda;
+	@FXML
+	private TextField tfCedulaBusqueda;
 
-    @FXML
-    private Button btnBuscar;
+	@FXML
+	private Button btnBuscar;
 
-    @FXML
-    private TableView<Client> tvInfoCliente;
+	@FXML
+	private TableView<Client> tvInfoCliente;
 
-    @FXML
-    private TableColumn<Client, String> tcNombre;
+	@FXML
+	private TableColumn<Client, String> tcNombre;
 
-    @FXML
-    private TableColumn<Client, Integer> tcCedula;
+	@FXML
+	private TableColumn<Client, Integer> tcCedula;
 
-    @FXML
-    private TableColumn<Client, String> tcCuentaBancaria;
+	@FXML
+	private TableColumn<Client, String> tcCuentaBancaria;
 
-    @FXML
-    private TableColumn<Client, Integer> tcTarjeta;
+	@FXML
+	private TableColumn<Client, Integer> tcTarjeta;
 
-    @FXML
-    private TableColumn<Client, String> tcFechaPago;
+	@FXML
+	private TableColumn<Client, String> tcFechaPago;
 
-    @FXML
-    private TableColumn<Client, String> tcIncorporacion;
-    
-    @FXML
-    private Tab tpRetiro, tpConsignación, tpPagoTarjeta, tpCancelacion;
-    
-    @FXML
-    private Text txtNumeroTarjeta, txtNombreTarjeta, noCuentaBancaria, dineroCuentaBancaria;
+	@FXML
+	private TableColumn<Client, String> tcIncorporacion;
 
-    @FXML
-    private TextField tfRetirarDinero, tfConsignarDinero ;
-    
-    @FXML
-    private Button btnConsignarDinero, btnRetirarDinero;
-    
-    private Bank bank;
-    
-    
-    
-    @FXML
-    void buscarClienteParaAtender(ActionEvent event) {
+	@FXML
+	private Tab tpRetiro, tpConsignación, tpPagoTarjeta, tpCancelacion;
+
+	@FXML
+	private Text txtNumeroTarjeta, txtNombreTarjeta, noCuentaBancaria, dineroCuentaBancaria;
+
+	@FXML
+	private TextField tfRetirarDinero, tfConsignarDinero ;
+
+	@FXML
+	private Button btnConsignarDinero, btnRetirarDinero;
+
+	private Bank bank;
+
+
+
+	@FXML
+	void buscarClienteParaAtender(ActionEvent event) {
 
 		actualizarInformacionCliente(bank.getEnAtencion());
 
-    	
-    }
+
+	}
 
 
-    
+
 	/**
-     * Este evento está encargado de consignar dinero a la cuenta bancaria de la persona que está siendo atendida.
-     * @param event
-     * @author Gustavo Villada
-     */
-    @FXML
-    void consignarDinero(ActionEvent event) {
+	 * Este evento está encargado de consignar dinero a la cuenta bancaria de la persona que está siendo atendida.
+	 * @param event
+	 * @author Gustavo Villada
+	 */
+	@FXML
+	void consignarDinero(ActionEvent event) {
+		try {
+			int consignacion=Integer.parseInt(tfConsignarDinero.getText().toString());
+			tfConsignarDinero.setText("");
+			int consignado=bank.getEnAtencion().consignarDinero(consignacion);
 
-    	int consignacion=Integer.parseInt(tfConsignarDinero.getText().toString());
-    	tfConsignarDinero.setText("");
-    	int consignado=bank.getEnAtencion().consignarDinero(consignacion);
-    	
-    	DecimalFormat formato = new DecimalFormat("$#,###.###");
-    	String valorFormateado = formato.format(consignado);
-    	
-    	Alert alert=new Alert(AlertType.INFORMATION);
-    	alert.setTitle("Transacción realizada");
-    	alert.setContentText("Consignación exitosa, ahora tienes "+valorFormateado);
-    	alert.showAndWait();
-    	
-    	actualizarInformacionCliente(bank.getEnAtencion());
-    }
+			DecimalFormat formato = new DecimalFormat("$#,###.###");
+			String valorFormateado = formato.format(consignado);
 
-    
+			Alert alert=new Alert(AlertType.INFORMATION);
+			alert.setTitle("Transacción realizada");
+			alert.setContentText("Consignación exitosa, ahora tienes "+valorFormateado);
+			alert.showAndWait();
+
+			actualizarInformacionCliente(bank.getEnAtencion());
+		}catch (NumberFormatException e) {
+			Alert alert=new Alert(AlertType.ERROR);
+			alert.setTitle("Digitaste mal");
+			alert.setContentText("SE HA DIGITADO UN VALOR ERRONEO, REVISA LO QUE ESCRIBISTE ");
+			alert.showAndWait();
+		}
+	}
+
+
 	/**
-     * Este evento está encargado de retirar dinero a la cuenta bancaria de la persona que está siendo atendida.
-     * @param event
-     * @author Gustavo Villada
-     */
-    @FXML
-    void retirarDinero(ActionEvent event) {
+	 * Este evento está encargado de retirar dinero a la cuenta bancaria de la persona que está siendo atendida.
+	 * @param event
+	 * @author Gustavo Villada
+	 */
+	@FXML
+	void retirarDinero(ActionEvent event) {
+		try {
+			int dineroARetirar=Integer.parseInt(tfRetirarDinero.getText().toString());
+			tfRetirarDinero.setText("");
+			boolean retirado=bank.getEnAtencion().retirarDinero(dineroARetirar);
+			if(retirado==false) {
+				Alert alert=new Alert(AlertType.INFORMATION);
+				alert.setTitle("Transacción fallida");
+				alert.setContentText("FONDOS INSUFICIENTES");
+				alert.showAndWait();
+			}else {
+				DecimalFormat formato = new DecimalFormat("$#,###.###");
+				String valorFormateado = formato.format(bank.getEnAtencion().getBankAccount().getAmmount());
 
-    	int dineroARetirar=Integer.parseInt(tfRetirarDinero.getText().toString());
-    	tfRetirarDinero.setText("");
-    	boolean retirado=bank.getEnAtencion().retirarDinero(dineroARetirar);
-    	if(retirado==false) {
-        	Alert alert=new Alert(AlertType.INFORMATION);
-        	alert.setTitle("Transacción fallida");
-        	alert.setContentText("FONDOS INSUFICIENTES");
-        	alert.showAndWait();
-    	}else {
-        	DecimalFormat formato = new DecimalFormat("$#,###.###");
-        	String valorFormateado = formato.format(bank.getEnAtencion().getBankAccount().getAmmount());
-        	
-        	Alert alert=new Alert(AlertType.INFORMATION);
-        	alert.setTitle("Transacción realizada");
-        	alert.setContentText("Retiro exitoso, ahora tienes "+valorFormateado);
-        	alert.showAndWait();
-        	
-        	actualizarInformacionCliente(bank.getEnAtencion());
-    	}
+				Alert alert=new Alert(AlertType.INFORMATION);
+				alert.setTitle("Transacción realizada");
+				alert.setContentText("Retiro exitoso, ahora tienes "+valorFormateado);
+				alert.showAndWait();
 
-    }
+				actualizarInformacionCliente(bank.getEnAtencion());
+			}
+		}catch (NumberFormatException e) {
+			Alert alert=new Alert(AlertType.ERROR);
+			alert.setTitle("Digitaste mal");
+			alert.setContentText("SE HA DIGITADO UN VALOR ERRONEO, REVISA LO QUE ESCRIBISTE ");
+			alert.showAndWait();
+		}
 
-    /**
-     * Este metodo recibe la clase principal del banco que viene de la ventana inicial.
-     * @param bank
-     * @author Gustavo Villada
-     */
+	}
+
+	/**
+	 * Este metodo recibe la clase principal del banco que viene de la ventana inicial.
+	 * @param bank
+	 * @author Gustavo Villada
+	 */
 	public void transferMainClass(Bank bank) {
 
 		this.bank=bank;
@@ -149,39 +161,39 @@ public class Attention_WindowController{
 			actualizarInformacionCliente(bank.getEnAtencion());
 		}
 	}
-	
-	
-	
-	/**
-     * Este evento está encargado de tomar un cliente de las filas, y pasarlo a un estado de "cliente pendiente", donde su información estará 
-     * visible en una tableView (Solo se podrá hacer este evento si no hay ningún "cliente pendiente").
-     * @param Cliente cuya información se va a mostrar en la tableView
-     * @author Gustavo Villada
-     */
-    private void actualizarInformacionCliente(Client c) {
 
-    	
+
+
+	/**
+	 * Este evento está encargado de tomar un cliente de las filas, y pasarlo a un estado de "cliente pendiente", donde su información estará 
+	 * visible en una tableView (Solo se podrá hacer este evento si no hay ningún "cliente pendiente").
+	 * @param Cliente cuya información se va a mostrar en la tableView
+	 * @author Gustavo Villada
+	 */
+	private void actualizarInformacionCliente(Client c) {
+
+
 		ArrayList<Client> clientPrev=new ArrayList<Client>();
 		clientPrev.add(c);
-	    ObservableList<Client> clientePr;
+		ObservableList<Client> clientePr;
 
-    	clientePr = FXCollections.observableArrayList(clientPrev);
-    	tvInfoCliente.setItems(clientePr);
+		clientePr = FXCollections.observableArrayList(clientPrev);
+		tvInfoCliente.setItems(clientePr);
 
-       	tcNombre.setCellValueFactory(new PropertyValueFactory<>("name"));
-    	tcCedula.setCellValueFactory(new PropertyValueFactory<>("cedula"));
-    	tcCuentaBancaria.setCellValueFactory(new PropertyValueFactory<>("bankAccount"));
-    	tcTarjeta.setCellValueFactory(new PropertyValueFactory<>("creditCards"));
-    	tcFechaPago.setCellValueFactory(new PropertyValueFactory<>("datePaymentCard"));
-    	tcIncorporacion.setCellValueFactory(new PropertyValueFactory<>("dateIncorporation"));
-    	
-    	txtNombreTarjeta.setText(c.getName());
-    	txtNumeroTarjeta.setText(c.getBankAccount().getId());
-    	noCuentaBancaria.setText(c.getBankAccount().getId());
-    	DecimalFormat formato = new DecimalFormat("$#,###.###");
-    	String valorFormateado = formato.format(c.getBankAccount().getAmmount());
-    	dineroCuentaBancaria.setText(valorFormateado);
-    	
+		tcNombre.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tcCedula.setCellValueFactory(new PropertyValueFactory<>("cedula"));
+		tcCuentaBancaria.setCellValueFactory(new PropertyValueFactory<>("bankAccount"));
+		tcTarjeta.setCellValueFactory(new PropertyValueFactory<>("creditCards"));
+		tcFechaPago.setCellValueFactory(new PropertyValueFactory<>("datePaymentCard"));
+		tcIncorporacion.setCellValueFactory(new PropertyValueFactory<>("dateIncorporation"));
+
+		txtNombreTarjeta.setText(c.getName());
+		txtNumeroTarjeta.setText(c.getBankAccount().getId());
+		noCuentaBancaria.setText(c.getBankAccount().getId());
+		DecimalFormat formato = new DecimalFormat("$#,###.###");
+		String valorFormateado = formato.format(c.getBankAccount().getAmmount());
+		dineroCuentaBancaria.setText(valorFormateado);
+
 	}
-	
+
 }
